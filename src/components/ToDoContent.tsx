c363a3da79c28f571d4f13d48904ad16ff3d4ce4
bilaapd <nabilapuspitadewi@mail.ugm.ac.id>
2024-11-26 01:57:30 +0700

    fix: unused

--- src/components/ToDoContent.tsx
+++ src/components/ToDoContent.tsx
@@ -8,6 +8,8 @@ import { groupProps } from "@/types/types";
 import { usePathname, useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { getTasksArray } from "@/utils/utils";
+import Calendar from "react-calendar";
+
 
 export const ToDoContent: React.FC = () => {
   const authContext = useAuthContext();
@@ -20,6 +22,20 @@ export const ToDoContent: React.FC = () => {
   const [selectedGroupID, setSelectedGroupID] = useState<groupProps>(
     mainContext?.group.find((group) => group.id === id) as groupProps
   );
+
+const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+const handleClickDay = (date: Date) => {
+  setSelectedDate(date);
+  handleEdit({
+    title: "",
+    deadline: date,
+    status: "uncompleted",
+    description: "",
+    _id: id,
+    type: "create",
+  });
+};
 
   const handleEdit = ({
     title,
